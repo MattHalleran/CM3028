52d1c91230b642dd4c6fd0b885c1cb25e774cc50
Haroldas <haro.lato@gmail.com>
2014-12-17 22:28:36 +0200

    comments - Haroldas

--- app/Model/Lecturer.php
+++ app/Model/Lecturer.php
@@ -1,5 +1,10 @@
 <?php
-
+/*
+* Author: Haroldas Latonas
+* Matric: 1205950
+* Date:   17 Dec 2014
+* Description: Lecturer database model
+*/
 class Lecturer extends AppModel {
 	var $name = "Lecturer";
 	var $primary_key = "staffID";
